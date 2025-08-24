6509e087f62609cc7a6cc9f1cad4ba9a7834ff1a
erkinyoltravel <erkinyoltravel@gmail.com>
2025-08-24 18:27:22 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,11 +8,12 @@ const nextConfig = {
           { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
           { key: 'X-Content-Type-Options', value: 'nosniff' },
           { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
-          { key: 'Permissions-Policy', value: 'geolocation=(), microphone=()' }
+          { key: 'Permissions-Policy', value: 'geolocation=(), microphone=()' },
         ],
       },
     ];
   },
+
   async redirects() {
     return [
       {
@@ -23,5 +24,14 @@ const nextConfig = {
       },
     ];
   },
+
+  // Включаем строгую проверку
+  eslint: {
+    ignoreDuringBuilds: false, // true = игнорировать ошибки на билде, false = падать при ошибках
+  },
+  typescript: {
+    ignoreBuildErrors: false, // true = игнорировать ошибки TS, false = падать при ошибках
+  },
 };
+
 module.exports = nextConfig;
